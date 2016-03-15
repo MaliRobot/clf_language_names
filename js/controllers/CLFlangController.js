f43384de49481ae77d9757812838e08af4ee0c0f
Milos Trifunovic <milos.trifunovic@gmail.com>
2016-03-15 12:53:19 +0100

    Modified views and controllers

--- js/controllers/CLFlangController.js
+++ js/controllers/CLFlangController.js
@@ -1,15 +1,16 @@
 app.controller('CLFlangController', function($scope, $http, $routeParams) { 
-  $scope.title = 'Names of languages'; 
-  $scope.lang = undefined;
-  $scope.langTitle = undefined;
+  $scope.title = 'Names of Wikipedia languages'; 
+  $scope.languages = undefined;
 
-  $http.get('js/clf.json')
-    .success(function (data) {
-        $scope.languages = data;                
-    })
-    .error(function(data,status,error,config){
-        $scope.languages = [{heading:"Error",description:"Could not load json data"}];
-    });
+  if($scope.languages === undefined){
+    $http.get('js/clf.json')
+      .success(function (data) {
+          $scope.languages = data;                
+      })
+      .error(function(data,status,error,config){
+          $scope.languages = [{heading:"Error",description:"Could not load json data"}];
+      });
+  };
 
   $scope.showLanguage = function(key) {
   	$scope.lang = $scope.languages[key];
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,22 +2,26 @@ app.controller('MainController', function($scope, $http, $routeParams) {
   $scope.title = 'Names of languages'; 
   $scope.search = undefined;  
   $scope.result = undefined;
+  $scope.languages = undefined;
 
-  $http.get('js/clf.json')
-    .success(function (data) {
-        $scope.languages = data;                
-    })
-    .error(function(data,status,error,config){
-        $scope.languages = [{heading:"Error",description:"Could not load json data"}];
-    });
+  if($scope.languages === undefined){
+    $http.get('js/clf.json')
+      .success(function (data) {
+          $scope.languages = data;                
+      })
+      .error(function(data,status,error,config){
+          $scope.languages = [{heading:"Error",description:"Could not load json data"}];
+      });
+  };
 
   $scope.findValue = function(search) { 
-  	console.log(search, $scope.search);    
+  	console.log($scope.languages, $scope.search);    
     angular.forEach($scope.languages, function(value, key) {
-        if (key === search) {
+        if (key.toLowerCase() === search.toLowerCase()) {
             $scope.result = $scope.languages[key];
         }
+        console.log($scope.result);
     });
   };
-  console.log($scope.result);
+  
 });
